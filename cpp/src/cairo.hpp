83b914aae25c369d66c4b3cc7631ba3a60d9ce64
Maximilian Luz <luzmaximilian@gmail.com>
2021-01-21 23:12:07 +0100

    cpp: Mark cairo wrapper functions as inline

--- cpp/src/cairo.hpp
+++ cpp/src/cairo.hpp
@@ -30,21 +30,6 @@ public:
     auto code() const noexcept -> status_t;
 };
 
-exception::exception(status_t code)
-    : m_code{code}
-{}
-
-auto exception::what() const noexcept -> const char*
-{
-    return cairo_status_to_string(m_code);
-}
-
-auto exception::code() const noexcept -> status_t
-{
-    return m_code;
-}
-
-
 enum class format {
     argb32   = CAIRO_FORMAT_ARGB32,
     rgba128f = CAIRO_FORMAT_RGBA128F,
@@ -182,30 +167,45 @@ public:
 };
 
 
-cairo::cairo()
+inline exception::exception(status_t code)
+    : m_code{code}
+{}
+
+inline auto exception::what() const noexcept -> const char*
+{
+    return cairo_status_to_string(m_code);
+}
+
+inline auto exception::code() const noexcept -> status_t
+{
+    return m_code;
+}
+
+
+inline cairo::cairo()
     : m_raw{nullptr}
 {}
 
-cairo::cairo(cairo_t* raw)
+inline cairo::cairo(cairo_t* raw)
     : m_raw{raw}
 {}
 
-cairo::cairo(cairo const& other)
+inline cairo::cairo(cairo const& other)
     : m_raw{other.m_raw ? cairo_reference(other.m_raw) : nullptr}
 {}
 
-cairo::cairo(cairo&& other)
+inline cairo::cairo(cairo&& other)
     : m_raw{std::exchange(other.m_raw, nullptr)}
 {}
 
-cairo::~cairo()
+inline cairo::~cairo()
 {
     if (m_raw) {
         cairo_destroy(m_raw);
     }
 }
 
-auto cairo::create(surface& target) -> cairo
+inline auto cairo::create(surface& target) -> cairo
 {
     cairo cr { cairo_create(target.raw()) };
 
@@ -216,122 +216,122 @@ auto cairo::create(surface& target) -> cairo
     return cr;
 }
 
-void cairo::operator=(cairo const& rhs)
+inline void cairo::operator=(cairo const& rhs)
 {
     m_raw = rhs.m_raw ? cairo_reference(rhs.m_raw) : nullptr;
 }
 
-void cairo::operator=(cairo&& rhs)
+inline void cairo::operator=(cairo&& rhs)
 {
     m_raw = std::exchange(rhs.m_raw, nullptr);
 }
 
-auto cairo::raw() -> cairo_t*
+inline auto cairo::raw() -> cairo_t*
 {
     return m_raw;
 }
 
-auto cairo::operator* () -> cairo_t*
+inline auto cairo::operator* () -> cairo_t*
 {
     return m_raw;
 }
 
-auto cairo::status() const -> cairo_status_t
+inline auto cairo::status() const -> cairo_status_t
 {
     return cairo_status(m_raw);
 }
 
-void cairo::set_source(pattern& p)
+inline void cairo::set_source(pattern& p)
 {
     cairo_set_source(m_raw, *p);
 }
 
-void cairo::set_source(cmap::srgb c)
+inline void cairo::set_source(cmap::srgb c)
 {
     cairo_set_source_rgb(m_raw, c.r, c.g, c.b);
 }
 
-void cairo::set_source(cmap::srgba c)
+inline void cairo::set_source(cmap::srgba c)
 {
     cairo_set_source_rgba(m_raw, c.r, c.g, c.b, c.a);
 }
 
-void cairo::set_source(surface& src, vec2<f64> origin)
+inline void cairo::set_source(surface& src, vec2<f64> origin)
 {
     cairo_set_source_surface(m_raw, *src, origin.x, origin.y);
 }
 
-auto cairo::get_source() -> pattern
+inline auto cairo::get_source() -> pattern
 {
     return { cairo_pattern_reference(cairo_get_source(m_raw)) };
 }
 
-void cairo::set_source_filter(filter f)
+inline void cairo::set_source_filter(filter f)
 {
     cairo_pattern_set_filter(cairo_get_source(m_raw), static_cast<cairo_filter_t>(f));
 }
 
-void cairo::paint()
+inline void cairo::paint()
 {
     cairo_paint(m_raw);
 }
 
-void cairo::fill()
+inline void cairo::fill()
 {
     cairo_fill(m_raw);
 }
 
-void cairo::stroke()
+inline void cairo::stroke()
 {
     cairo_stroke(m_raw);
 }
 
-void cairo::save()
+inline void cairo::save()
 {
     cairo_save(m_raw);
 }
 
-void cairo::restore()
+inline void cairo::restore()
 {
     cairo_restore(m_raw);
 }
 
-void cairo::translate(vec2<f64> v)
+inline void cairo::translate(vec2<f64> v)
 {
     cairo_translate(m_raw, v.x, v.y);
 }
 
-void cairo::scale(vec2<f64> s)
+inline void cairo::scale(vec2<f64> s)
 {
     cairo_scale(m_raw, s.x, s.y);
 }
 
-void cairo::rotate(f64 angle)
+inline void cairo::rotate(f64 angle)
 {
     cairo_rotate(m_raw, angle);
 }
 
-void cairo::move_to(vec2<f64> pos)
+inline void cairo::move_to(vec2<f64> pos)
 {
     cairo_move_to(m_raw, pos.x, pos.y);
 }
 
-void cairo::line_to(vec2<f64> pos)
+inline void cairo::line_to(vec2<f64> pos)
 {
     cairo_line_to(m_raw, pos.x, pos.y);
 }
 
-void cairo::rectangle(vec2<f64> origin, vec2<f64> size)
+inline void cairo::rectangle(vec2<f64> origin, vec2<f64> size)
 {
     cairo_rectangle(m_raw, origin.x, origin.y, size.x, size.y);
 }
 
-void cairo::arc(vec2<f64> center, f64 radius, f64 angle1, f64 angle2)
+inline void cairo::arc(vec2<f64> center, f64 radius, f64 angle1, f64 angle2)
 {
     cairo_arc(m_raw, center.x, center.y, radius, angle1, angle2);
 }
 
-void cairo::select_font_face(char const* family, font_slant slant, font_weight weight)
+inline void cairo::select_font_face(char const* family, font_slant slant, font_weight weight)
 {
     auto const s = static_cast<cairo_font_slant_t>(slant);
     auto const w = static_cast<cairo_font_weight_t>(weight);
@@ -339,66 +339,66 @@ void cairo::select_font_face(char const* family, font_slant slant, font_weight w
     cairo_select_font_face(m_raw, family, s, w);
 }
 
-void cairo::set_font_size(f64 size)
+inline void cairo::set_font_size(f64 size)
 {
     cairo_set_font_size(m_raw, size);
 }
 
-void cairo::show_text(char const* utf8)
+inline void cairo::show_text(char const* utf8)
 {
     cairo_show_text(m_raw, utf8);
 }
 
 
-surface::surface()
+inline surface::surface()
     : m_raw{nullptr}
 {}
 
-surface::surface(cairo_surface_t* raw)
+inline surface::surface(cairo_surface_t* raw)
     : m_raw{raw}
 {}
 
-surface::surface(surface const& other)
+inline surface::surface(surface const& other)
     : m_raw{other.m_raw ? cairo_surface_reference(other.m_raw) : nullptr}
 {}
 
-surface::surface(surface&& other)
+inline surface::surface(surface&& other)
     : m_raw{std::exchange(other.m_raw, nullptr)}
 {}
 
-surface::~surface()
+inline surface::~surface()
 {
     if (m_raw) {
         cairo_surface_destroy(m_raw);
     }
 }
 
-void surface::operator=(surface const& rhs)
+inline void surface::operator=(surface const& rhs)
 {
     m_raw = rhs.m_raw ? cairo_surface_reference(rhs.m_raw) : nullptr;
 }
 
-void surface::operator=(surface&& rhs)
+inline void surface::operator=(surface&& rhs)
 {
     m_raw = std::exchange(rhs.m_raw, nullptr);
 }
 
-auto surface::raw() -> cairo_surface_t*
+inline auto surface::raw() -> cairo_surface_t*
 {
     return m_raw;
 }
 
-auto surface::operator* () -> cairo_surface_t*
+inline auto surface::operator* () -> cairo_surface_t*
 {
     return m_raw;
 }
 
-auto surface::status() const -> cairo_status_t
+inline auto surface::status() const -> cairo_status_t
 {
     return cairo_surface_status(m_raw);
 }
 
-void surface::write_to_png(char const* filename)
+inline void surface::write_to_png(char const* filename)
 {
     status_t status;
 
@@ -408,107 +408,107 @@ void surface::write_to_png(char const* filename)
     }
 }
 
-void surface::write_to_png(std::filesystem::path const& p)
+inline void surface::write_to_png(std::filesystem::path const& p)
 {
     write_to_png(p.c_str());
 }
 
 
-pattern::pattern()
+inline pattern::pattern()
     : m_raw{}
 {}
 
-pattern::pattern(cairo_pattern_t* raw)
+inline pattern::pattern(cairo_pattern_t* raw)
     : m_raw{raw}
 {}
 
-pattern::pattern(pattern const& other)
+inline pattern::pattern(pattern const& other)
     : m_raw{other.m_raw ? cairo_pattern_reference(other.m_raw) : nullptr}
 {}
 
-pattern::pattern(pattern&& other)
+inline pattern::pattern(pattern&& other)
     : m_raw{std::exchange(other.m_raw, nullptr)}
 {}
 
-pattern::~pattern()
+inline pattern::~pattern()
 {
     if (m_raw) {
         cairo_pattern_destroy(m_raw);
     }
 }
 
-auto pattern::create_for_surface(surface &surface) -> pattern
+inline auto pattern::create_for_surface(surface &surface) -> pattern
 {
     return { cairo_pattern_create_for_surface(*surface) };
 }
 
-void pattern::operator=(pattern const& rhs)
+inline void pattern::operator=(pattern const& rhs)
 {
     m_raw = rhs.m_raw ? cairo_pattern_reference(rhs.m_raw) : nullptr;
 }
 
-void pattern::operator=(pattern&& rhs)
+inline void pattern::operator=(pattern&& rhs)
 {
     m_raw = std::exchange(rhs.m_raw, nullptr);
 }
 
-auto pattern::raw() -> cairo_pattern_t*
+inline auto pattern::raw() -> cairo_pattern_t*
 {
     return m_raw;
 }
 
-auto pattern::operator* () -> cairo_pattern_t*
+inline auto pattern::operator* () -> cairo_pattern_t*
 {
     return m_raw;
 }
 
-auto pattern::status() const -> status_t
+inline auto pattern::status() const -> status_t
 {
     return cairo_pattern_status(m_raw);
 }
 
-void pattern::set_matrix(matrix &m)
+inline void pattern::set_matrix(matrix &m)
 {
     return cairo_pattern_set_matrix(m_raw, *m);
 }
 
-void pattern::set_filter(filter f)
+inline void pattern::set_filter(filter f)
 {
     return cairo_pattern_set_filter(m_raw, static_cast<cairo_filter_t>(f));
 }
 
 
-matrix::matrix()
+inline matrix::matrix()
     : m_raw{}
 {}
 
-matrix::matrix(cairo_matrix_t m)
+inline matrix::matrix(cairo_matrix_t m)
     : m_raw{m}
 {}
 
-auto matrix::identity() -> matrix
+inline auto matrix::identity() -> matrix
 {
     auto m = matrix{};
     cairo_matrix_init_identity(&m.m_raw);
     return m;
 }
 
-auto matrix::raw() -> cairo_matrix_t*
+inline auto matrix::raw() -> cairo_matrix_t*
 {
     return &m_raw;
 }
 
-auto matrix::operator* () -> cairo_matrix_t*
+inline auto matrix::operator* () -> cairo_matrix_t*
 {
     return &m_raw;
 }
 
-void matrix::translate(vec2<f64> v)
+inline void matrix::translate(vec2<f64> v)
 {
     cairo_matrix_translate(&m_raw, v.x, v.y);
 }
 
-void matrix::scale(vec2<f64> s)
+inline void matrix::scale(vec2<f64> s)
 {
     cairo_matrix_scale(&m_raw, s.x, s.y);
 }
@@ -518,13 +518,13 @@ template<typename T>
 constexpr auto pixel_format() -> format;
 
 template<>
-constexpr auto pixel_format<cmap::srgba>() -> format
+inline constexpr auto pixel_format<cmap::srgba>() -> format
 {
     return format::rgba128f;
 }
 
 
-auto image_surface_create(format fmt, vec2<i32> shape) -> surface
+inline auto image_surface_create(format fmt, vec2<i32> shape) -> surface
 {
     surface s { cairo_image_surface_create(static_cast<cairo_format_t>(fmt), shape.x, shape.y) };
 
@@ -535,7 +535,7 @@ auto image_surface_create(format fmt, vec2<i32> shape) -> surface
     return s;
 }
 
-auto format_stride_for_width(format fmt, int width) -> int
+inline auto format_stride_for_width(format fmt, int width) -> int
 {
     int stride = cairo_format_stride_for_width(static_cast<cairo_format_t>(fmt), width);
 
@@ -547,7 +547,7 @@ auto format_stride_for_width(format fmt, int width) -> int
 }
 
 template<typename T>
-auto image_surface_create(image<T>& image) -> surface
+inline auto image_surface_create(image<T>& image) -> surface
 {
     auto const format = pixel_format<T>();
     auto const shape = image.shape();
